976c29ab993a75992e30ce4f469ab7abe2b3e093
Juhamikael <contact.juhamusic@gmail.com>
2022-05-04 20:37:01 +0300

    Making 2 new functions: void printChoiceMenu & void foodNotFound

--- C++/Funcs.cpp
+++ C++/Funcs.cpp
@@ -31,3 +31,30 @@ int checkInputErrors() {
     }
     return choice;
 }
+
+void printChoiceMenu(int menu) {
+    if (menu == 1) {
+        cout << "-----------------------\n"
+                "1. Add new food\n"
+                "2. Remove food\n"
+                "3. Print all foods\n"
+                "4. Search for food\n"
+                "5. Sort foods by calories\n"
+                "6. Change details of food\n"
+                "7. Exit\n";
+        cout << "Enter your choice:";
+    } else if (menu == 2) {
+        cout << "\n1. Change food name\n"
+                "2. Change food protein value\n"
+                "3. Change food carb value\n"
+                "4. Change food fat value\n"
+                "5. Print food values\n"
+                "6. Go back to main menu\n";
+    }
+}
+
+void foodNotFound(bool found, const string &name) {
+    if (!found) {
+        cout << "\nFood with name '" << name << "' not found! :(\n\n";
+    }
+}
