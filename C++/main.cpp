d2492c740f2c99f2dc5f5f792da2e9f97034ed60
Juhamikael <contact.juhamusic@gmail.com>
2022-05-04 20:39:29 +0300

    Change to dynamic memory management & refactoring

--- C++/main.cpp
+++ C++/main.cpp
@@ -2,17 +2,19 @@
 #include "Funcs.h"
 
 int main() {
-    vector<Food *> foods;
+    vector<unique_ptr<Food>> foods;
 
-    // Adding placeholder data////////
+    // Adding placeholder data/////////
+    // ////////////////////////////////
     cout << "-----------------------\nAdding placeholder data to vector\n";
-    foods.push_back(new Food("Jauheliha", 20, 0, 10));
-    foods.push_back(new Food("Kana", 18, 0, 0));
-    foods.push_back(new Food("Kala", 20, 0, 20));
-    foods.push_back(new Food("Suklaa", 11, 49, 33));
+    foods.push_back(make_unique<Food>("Jauheliha", 20, 0, 10));
+    foods.push_back(make_unique<Food>("Kana", 18, 0, 0));
+    foods.push_back(make_unique<Food>("Kala", 20, 0, 20));
+    foods.push_back(make_unique<Food>("Suklaa", 11, 49, 33));
     cout << foods.size() << " foods added \n-----------------------\n\n\n";
-    // ////////
+    // ////////////////////////////////
 
+    // ////////////////////////////////
     // Global Variables
     string nameToRemove;
     string nameToSearch;
@@ -21,42 +23,33 @@ int main() {
     int setChangeFoodChoice;
     string newFoodName;
     int newValue;
+    // ////////////////////////////////
 
-
+    // ////////////////////////////////
+    // Start of program
     cout << "Welcome to the food database!\n";
     do {
-        cout << "-----------------------\n"
-                "1. Add new food\n"
-                "2. Remove food\n"
-                "3. Print all foods\n"
-                "4. Search for food\n"
-                "5. Sort foods by calories\n"
-                "6. Change details of food\n"
-                "7. Exit\n";
-        cout << "Enter your choice:";
+        printChoiceMenu(1);
         choice = checkInputErrors();
-
         switch (choice) {
             case 1:
-                foods.push_back(new Food(AddNewFood()));
+                foods.push_back(make_unique<Food>(AddNewFood()));
                 break;
             case 2:
                 cout << "Enter food name to remove: ";
                 cin >> nameToRemove;
-                for (Food *i: foods) {
+                for (auto &i: foods) {
                     if (i->getName() == nameToRemove) {
                         foods.erase(find(foods.begin(), foods.end(), i));
                         foodFound = true;
                         break;
                     }
                 }
-                if (!foodFound) {
-                    foodFound = false;
-                    cout << "Food not found!\n";
-                }
+                foodNotFound(foodFound, nameToRemove);
+                foodFound = false;
                 break;
             case 3:
-                cout << "\n\n-------\nFoods found: " << foods.size() << "\n-------";
+                cout << "\n\n-------\nFood found: " << foods.size() << "\n-------";
                 for (auto &&food: foods) {
                     food->printFood();
                 }
@@ -66,7 +59,7 @@ int main() {
                 cin >> nameToSearch;
                 nameToSearch = StringToLower(nameToSearch);
                 // Iterate through vector and get index if found
-                for (Food *i: foods) {
+                for (auto &i: foods) {
                     if (StringToLower(i->getName()) == nameToSearch) {
                         foodFound = true;
                         cout << "Food with name '" << i->getName() << "' found!\n";
@@ -74,14 +67,11 @@ int main() {
                         break;
                     }
                 }
-                if (!foodFound) {
-                    foodFound = false;
-
-                    cout << "\nFood with name '" << nameToSearch << "' not found! :(\n\n";
-                    break;
-                }
+                foodNotFound(foodFound, nameToRemove);
+                foodFound = false;
+                break;
             case 5:
-                sort(foods.begin(), foods.end(), [](Food *a, Food *b) {
+                sort(foods.begin(), foods.end(), [](const unique_ptr<Food> &a, const unique_ptr<Food> &b) {
                     return a->getCalories() < b->getCalories();
                 });
                 break;
@@ -89,41 +79,36 @@ int main() {
                 cout << "Enter food name search: ";
                 cin >> nameToSearch;
                 nameToSearch = StringToLower(nameToSearch);
-                for (int i = 0; i < foods.size(); i++) {
-                    if (StringToLower(foods[i]->getName()) == nameToSearch) {
-                        cout << "\nFood with name '" << foods[i]->getName() << "' found!\n";
+                for (auto &i: foods) {
+                    if (StringToLower(i->getName()) == nameToSearch) {
+                        cout << "\nFood with name '" << i->getName() << "' found!\n";
                         foodFound = true;
                         do {
-                            cout << "\n1. Change food name\n"
-                                    "2. Change food protein value\n"
-                                    "3. Change food carb value\n"
-                                    "4. Change food fat value\n"
-                                    "5. Print food values\n"
-                                    "6. Go back to main menu\n";
+                            printChoiceMenu(2);
                             setChangeFoodChoice = checkInputErrors();
                             switch (setChangeFoodChoice) {
                                 case 1:
                                     cout << "Give new name:\n";
                                     cin >> newFoodName;
-                                    foods[i]->setName(newFoodName);
+                                    i->setName(newFoodName);
                                     break;
                                 case 2:
                                     cout << "Give new Protein value:\n";
                                     cin >> newValue;
-                                    foods[i]->setProtein(newValue);
+                                    i->setProtein(newValue);
                                     break;
                                 case 3:
                                     cout << "Give new Carb value:\n";
                                     cin >> newValue;
-                                    foods[i]->setCarbs(newValue);
+                                    i->setCarbs(newValue);
                                     break;
                                 case 4:
                                     cout << "Give new Fat value:\n";
                                     cin >> newValue;
-                                    foods[i]->setFat(newValue);
+                                    i->setFat(newValue);
                                     break;
                                 case 5:
-                                    foods[i]->printFood();
+                                    i->printFood();
                                     break;
                                 case 6:
                                     cout << "Going back go main menu...\n";
@@ -132,16 +117,13 @@ int main() {
                         } while (setChangeFoodChoice != 6);
                     }
                 }
-                if (!foodFound) {
-                    cout << "\nFood with name '" << nameToSearch << "' not found! :(\n\n";
-                }
+                foodNotFound(foodFound, nameToRemove);
                 foodFound = false;
                 break;
             case 7:
+                foods.clear();
+                cout << "Food list cleared!\n\n";
                 cout << "Exiting...\n";
-                for (auto &food: foods) {
-                    delete food;
-                }
                 break;
             default:
                 cout << "Invalid input, try again..." << endl;
